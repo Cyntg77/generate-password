2f37f31b47878252dc2ea83deed9a1832b7428a8
Cynthia <142689354+Cyntg77@users.noreply.github.com>
2023-09-26 00:00:48 -0500

    generate-password

---
+++ script.js
@@ -0,0 +1,50 @@
+// Assignment code here
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+ // Write password to the #password input
+function writePassword() {
+  let passwordText = document.querySelector("#password");
+  let length = Number(prompt("Enter the length of your password between 8-128 characters."))
+  let charType = prompt("Enter 'uppercase, lowercase, numbers, symbols' to include. ")
+
+ 
+  passwordText.value = password(length, charType);
+
+  function password(length, charType) {
+    let charGen = {
+      lowercase: 'abcdefghijklmnop',
+      uppercase: 'ABCDEFGHIJKLMNOP',
+      numbers: '0123456789',
+      symbols: '!@#$%^&*()-_=+',
+    };
+    
+    var charTypes = charType.toLowerCase().split(', ');
+    charSet = "";
+    for(var i=0; i < charTypes.length; i++) {
+      charSet += charGen[charTypes[i]];
+    }
+    
+    console.log(charSet);
+
+    var retVal = "";
+    
+    for (var i = 0; i < length; i++) {
+      retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+    }
+    return retVal;
+  }
+}
+
+
+
+
+function copyPassword() {
+  document.getElementById("password").select();
+  navigator.clipboard.writeText(copyText.value);
+  alert("Password copied to clipboard!" + copyText.value);
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
